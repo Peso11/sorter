2000e106428fbd3272506eb4118e7c95f6143f5c
Peso11 <36646428+Peso11@users.noreply.github.com>
2018-02-22 00:03:31 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,31 +1,44 @@
 class Sorter {
-  constructor() {
-    // your implementation
-  }
+    constructor() {
+        this.massiv = [];
+        this.comparator = function(a, b) {
+          return a - b;
+        }
+    }
 
-  add(element) {
-    // your implementation
-  }
+    add(element) {
+        this.massiv.push(element);
+    }
 
-  at(index) {
-    // your implementation
-  }
+    at(index) {
+        return this.massiv[index];
+    }
 
-  get length() {
-    // your implementation
-  }
+    get length() {
+        return this.massiv.length;
+    }
 
-  toArray() {
-    // your implementation
-  }
+    toArray() {
+        return this.massiv;
+    }
 
-  sort(indices) {
-    // your implementation
-  }
+    sort(indices) {
+        var temp = [];
+        for (var i = 0; i < indices.length; i++) {
+            temp.push(this.massiv[indices[i]]);
+        }
+        indices.sort();
+        temp.sort(this.comparator);
+        for (var i = 0; i < indices.length; i++) {
+            this.massiv[indices[i]] = temp[i];
+        }
 
-  setComparator(compareFunction) {
-    // your implementation
-  }
+    }
+
+    setComparator(compareFunction) {
+        this.comparator = compareFunction;
+    }
 }
+
 
 module.exports = Sorter;
